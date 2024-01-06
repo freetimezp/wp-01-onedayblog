bafbe16727c1aa5c0cfb84bb9096f315b1426ca6
freetimezp <kraftukrnet@ukr.net>
2024-01-06 18:55:58 +0200

    16. wp-01-onedayblog. Widget Slider. Custom image, title, text.

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+use function PHPSTORM_META\map;
+
 add_action('init', function () {
     //add font awesome style
     wp_enqueue_style('fontawesome', get_template_directory_uri() . '/assets/css/all.min.css', array(), '', false);
@@ -73,6 +75,227 @@ add_action('after_setup_theme', function () {
 
     add_theme_support('custom-logo', $defaults);
 });
+
+
+add_action('customize_register', function ($wp_customize) {
+    //image slider section
+    $wp_customize->add_section('theme_slider_settings', array(
+        'title' => 'Slider Settings',
+        'description' => 'Add slider settings here',
+        'priority' => 160,
+        'capability' => 'edit_theme_options',
+    ));
+
+    //activate slider settings
+    $wp_customize->add_setting('onedayblog_home_slide_activate', array(
+        'type' => 'theme_mod', // or 'option'
+        'capability' => 'edit_theme_options',
+        'default' => '1',
+        'transport' => 'refresh', // or postMessage
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control('onedayblog_home_slide_activate', array(
+        'type' => 'checkbox',
+        'section' => 'theme_slider_settings', // Required, core or custom.
+        'label' => __('Activate Image Slider'),
+        'description' => __('Activate or deactivate Image Slider'),
+        'input_attrs' => array(
+            'class' => 'my-custom-class-for-js',
+            'style' => '',
+        ),
+    ));
+
+    //slide 1
+    $wp_customize->add_setting('onedayblog_home_slide_1', array(
+        'type' => 'theme_mod', // or 'option'
+        'capability' => 'edit_theme_options',
+        'default' => '',
+        'transport' => 'refresh', // or postMessage
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control(
+        new WP_Customize_Cropped_Image_Control(
+            $wp_customize,
+            'onedayblog_home_slide_1',
+            array(
+                'label'      => 'Image Slide 1',
+                'section'    => 'theme_slider_settings',
+                'height' => 800, // cropper Height
+                'width' => 1200, // Cropper Width
+                'flex_width' => false, //Flexible Width
+                'flex_height' => false, // Flexible Heiht
+            )
+        )
+    );
+
+    //slide 1 text settings
+    $wp_customize->add_setting('onedayblog_home_slide_header_text_1', array(
+        'type' => 'theme_mod', // or 'option'
+        'capability' => 'edit_theme_options',
+        'default' => '',
+        'transport' => 'refresh', // or postMessage
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_setting('onedayblog_home_slide_content_text_1', array(
+        'type' => 'theme_mod', // or 'option'
+        'capability' => 'edit_theme_options',
+        'default' => '',
+        'transport' => 'refresh', // or postMessage
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control('onedayblog_home_slide_header_text_1', array(
+        'type' => 'text',
+        'section' => 'theme_slider_settings', // Required, core or custom.
+        'label' => __('Slide 1 title text'),
+        'input_attrs' => array(
+            'class' => 'my-custom-class-for-js',
+            'style' => '',
+            'placeholder' => __('title text here'),
+        ),
+    ));
+
+    $wp_customize->add_control('onedayblog_home_slide_content_text_1', array(
+        'type' => 'textarea',
+        'section' => 'theme_slider_settings', // Required, core or custom.
+        'label' => __('Slide 1 content text'),
+        'input_attrs' => array(
+            'class' => 'my-custom-class-for-js',
+            'style' => '',
+            'placeholder' => __('content text here'),
+        ),
+    ));
+
+    //slide 2
+    $wp_customize->add_setting('onedayblog_home_slide_2', array(
+        'type' => 'theme_mod', // or 'option'
+        'capability' => 'edit_theme_options',
+        'default' => '',
+        'transport' => 'refresh', // or postMessage
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control(
+        new WP_Customize_Cropped_Image_Control(
+            $wp_customize,
+            'onedayblog_home_slide_2',
+            array(
+                'label'      => 'Image Slide 2',
+                'section'    => 'theme_slider_settings',
+                'height' => 800, // cropper Height
+                'width' => 1200, // Cropper Width
+                'flex_width' => false, //Flexible Width
+                'flex_height' => false, // Flexible Heiht
+            )
+        )
+    );
+
+    //slide 2 text settings
+    $wp_customize->add_setting('onedayblog_home_slide_header_text_2', array(
+        'type' => 'theme_mod', // or 'option'
+        'capability' => 'edit_theme_options',
+        'default' => '',
+        'transport' => 'refresh', // or postMessage
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_setting('onedayblog_home_slide_content_text_2', array(
+        'type' => 'theme_mod', // or 'option'
+        'capability' => 'edit_theme_options',
+        'default' => '',
+        'transport' => 'refresh', // or postMessage
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control('onedayblog_home_slide_header_text_2', array(
+        'type' => 'text',
+        'section' => 'theme_slider_settings', // Required, core or custom.
+        'label' => __('Slide 2 title text'),
+        'input_attrs' => array(
+            'class' => 'my-custom-class-for-js',
+            'style' => '',
+            'placeholder' => __('title text here'),
+        ),
+    ));
+
+    $wp_customize->add_control('onedayblog_home_slide_content_text_2', array(
+        'type' => 'textarea',
+        'section' => 'theme_slider_settings', // Required, core or custom.
+        'label' => __('Slide 2 content text'),
+        'input_attrs' => array(
+            'class' => 'my-custom-class-for-js',
+            'style' => '',
+            'placeholder' => __('content text here'),
+        ),
+    ));
+
+    //slide 3
+    $wp_customize->add_setting('onedayblog_home_slide_3', array(
+        'type' => 'theme_mod', // or 'option'
+        'capability' => 'edit_theme_options',
+        'default' => '',
+        'transport' => 'refresh', // or postMessage
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control(
+        new WP_Customize_Cropped_Image_Control(
+            $wp_customize,
+            'onedayblog_home_slide_3',
+            array(
+                'label'      => 'Image Slide 3',
+                'section'    => 'theme_slider_settings',
+                'height' => 800, // cropper Height
+                'width' => 1200, // Cropper Width
+                'flex_width' => false, //Flexible Width
+                'flex_height' => false, // Flexible Heiht
+            )
+        )
+    );
+
+    //slide 3 text settings
+    $wp_customize->add_setting('onedayblog_home_slide_header_text_3', array(
+        'type' => 'theme_mod', // or 'option'
+        'capability' => 'edit_theme_options',
+        'default' => '',
+        'transport' => 'refresh', // or postMessage
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_setting('onedayblog_home_slide_content_text_3', array(
+        'type' => 'theme_mod', // or 'option'
+        'capability' => 'edit_theme_options',
+        'default' => '',
+        'transport' => 'refresh', // or postMessage
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control('onedayblog_home_slide_header_text_3', array(
+        'type' => 'text',
+        'section' => 'theme_slider_settings', // Required, core or custom.
+        'label' => __('Slide 3 title text'),
+        'input_attrs' => array(
+            'class' => 'my-custom-class-for-js',
+            'style' => '',
+            'placeholder' => __('title text here'),
+        ),
+    ));
+
+    $wp_customize->add_control('onedayblog_home_slide_content_text_3', array(
+        'type' => 'textarea',
+        'section' => 'theme_slider_settings', // Required, core or custom.
+        'label' => __('Slide 3 content text'),
+        'input_attrs' => array(
+            'class' => 'my-custom-class-for-js',
+            'style' => '',
+            'placeholder' => __('content text here'),
+        ),
+    ));
+});
+
 
 require get_template_directory() . '/template-parts/walker.php';
 require get_template_directory() . '/template-parts/widgets.php';
--- template-parts/slider.php
+++ template-parts/slider.php
@@ -1,38 +1,89 @@
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img class="d-block w-100" src="<?= get_template_directory_uri() . '/assets/images/picsum-001.jpg' ?>" alt="slide">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Title</h5>
-                <p>Text lorem</p>
+<?php
+$slider_active = get_theme_mod('onedayblog_home_slide_activate', 1);
+?>
+
+<?php if ($slider_active) : ?>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <?php
+                $image = get_template_directory_uri() . '/assets/images/picsum-001.jpg';
+                //get image from slider section in WP
+                if (get_theme_mod('onedayblog_home_slide_1', '') != "") {
+                    $image = wp_get_attachment_url(get_theme_mod('onedayblog_home_slide_1', ''));
+                }
+                ?>
+                <img class="d-block w-100" src="<?= $image ?>" alt="slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <?php
+                    $slider_title = get_theme_mod('onedayblog_home_slide_header_text_1', 0);
+                    $slider_content = get_theme_mod('onedayblog_home_slide_content_text_1', 0);
+                    ?>
+                    <h5>
+                        <?= $slider_title ?>
+                    </h5>
+                    <p>
+                        <?= $slider_content ?>
+                    </p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <?php
+                $image = get_template_directory_uri() . '/assets/images/picsum-002.jpg';
+                //get image from slider section in WP
+                if (get_theme_mod('onedayblog_home_slide_2', '') != "") {
+                    $image = wp_get_attachment_url(get_theme_mod('onedayblog_home_slide_2', ''));
+                }
+                ?>
+                <img class="d-block w-100" src="<?= $image ?>" alt="slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <?php
+                    $slider_title = get_theme_mod('onedayblog_home_slide_header_text_2', 0);
+                    $slider_content = get_theme_mod('onedayblog_home_slide_content_text_2', 0);
+                    ?>
+                    <h5>
+                        <?= $slider_title ?>
+                    </h5>
+                    <p>
+                        <?= $slider_content ?>
+                    </p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <?php
+                $image = get_template_directory_uri() . '/assets/images/picsum-003.jpg';
+                //get image from slider section in WP
+                if (get_theme_mod('onedayblog_home_slide_3', '') != "") {
+                    $image = wp_get_attachment_url(get_theme_mod('onedayblog_home_slide_3', ''));
+                }
+                ?>
+                <img class="d-block w-100" src="<?= $image ?>" alt="slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <?php
+                    $slider_title = get_theme_mod('onedayblog_home_slide_header_text_3', 0);
+                    $slider_content = get_theme_mod('onedayblog_home_slide_content_text_3', 0);
+                    ?>
+                    <h5>
+                        <?= $slider_title ?>
+                    </h5>
+                    <p>
+                        <?= $slider_content ?>
+                    </p>
+                </div>
             </div>
         </div>
-        <div class="carousel-item">
-            <img class="d-block w-100" src="<?= get_template_directory_uri() . '/assets/images/picsum-002.jpg' ?>" alt="slide">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Title</h5>
-                <p>Text lorem</p>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <img class="d-block w-100" src="<?= get_template_directory_uri() . '/assets/images/picsum-003.jpg' ?>" alt="slide">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Title</h5>
-                <p>Text lorem</p>
-            </div>
-        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
+<?php endif; ?>
